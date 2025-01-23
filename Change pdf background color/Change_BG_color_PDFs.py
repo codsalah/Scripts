d89df18dab14a1011eed2102351bb66148f6852a
codsalah <salahhamada63@gmail.com>
2025-01-24 01:52:45 +0200

    optimized for big pdfs

--- Change pdf background color/Change_BG_color_PDFs.py
+++ Change pdf background color/Change_BG_color_PDFs.py
@@ -1,15 +1,15 @@
 import fitz  # PyMuPDF
 
 # Example usage
-input_pdf_path = "/home/codsalah/Downloads/Logic - Logical Fallacies.pdf"  
-output_pdf_path = "/home/codsalah/Downloads/Logic - Logical Fallacies2.pdf" 
+input_pdf_path = "/home/codsalah/Documents/DDIA1.pdf"  
+output_pdf_path = "/home/codsalah/Documents/DDIA5.pdf"
 
-background_color = (200/255, 210/255, 180/255) 
-doc = fitz.open(input_pdf_path)
-for page in doc:
-    page.draw_rect(page.rect, color=None, fill=background_color, overlay=False)
+background_color = (200 / 255, 210 / 255, 180 / 255)  # Define background color once
+with fitz.open(input_pdf_path) as doc:
+    for page in doc:
+        # Apply background fill in one step instead of drawing a rectangle
+        page.draw_rect(page.rect, color=None, fill=background_color, overlay=False)
 
-doc.ez_save(output_pdf_path)
-doc.close()
+    doc.save(output_pdf_path)
+
 print("Background color changed and saved to", output_pdf_path)
-
